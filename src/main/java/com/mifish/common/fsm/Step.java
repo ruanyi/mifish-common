9a2781efa6408203cc3d968f8d6f2141007aa41d
ruanlongsheng <ruanlongsheng@foxmail.com>
2017-12-10 00:19:54 +0800

    modify life fsm to mifish framework

--- src/main/java/com/mifish/common/fsm/Step.java
+++ src/main/java/com/mifish/common/fsm/Step.java
@@ -3,7 +3,15 @@ package com.mifish.common.fsm;
 import com.mifish.common.fsm.model.Society;
 
 /**
- * Description:
+ * 约定：
+ * 1、所有step必须是无状态的。
+ * 2、step与step之间的数据传递是通过：pfopContext里的attributes。记住：这种传递是临时的
+ * 3、当整体流程已经结束时，返回的NEXT_STEP 是：FINISH
+ * 4、每一个step类：最好不好超过300行，里面的step方法，不超过：50行。假如超过了，则建议：重新思考step拆分是否合理
+ * 5、抽象跟分解：一个工作流的步骤整合和拆分，很关键，否则，这套框架又会废了
+ * 6、目前step与step之间暂不支持多线程跑，后续根据实际情况再进行考虑
+ * 7、每个step必须具有唯一的名称
+ * 8、一个step尽量少些 if else,如果场景不一样，那最好新写一个step，后续可以把废弃的step直接删除，避免if else过多，看代码时头都疼死了
  *
  * @author: rls
  * Date: 2017-12-09 23:59
